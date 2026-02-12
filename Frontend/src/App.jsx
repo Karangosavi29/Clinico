63892eee64bb1f539c8271b9b7347fcbdf8a1761
karangosavi29 <karangosavi2906@gmail.com>
2026-02-12 12:34:01 +0530

    feat: add routing for login, signup, and protected admin/doctor/patient dashboards

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,42 @@
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
+import Login from "./pages/auth/login";
+import Signup from "./pages/auth/Signup";
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  
 
+const App = () => {
   return (
-    <>
-      <h1>Hello || my frontend project</h1>
-       
-    </>
-  )
-}
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute role="admin">
+            <div>Admin Dashboard</div>
+          </ProtectedRoute>
+        }
+      />
+       <Route
+        path="/doctor"
+        element={
+          <ProtectedRoute role="doctor">
+            <div>Doctor Dashboard</div>
+          </ProtectedRoute>
+        }
+      />
 
-export default App
+       <Route
+        path="/patient"
+        element={
+          <ProtectedRoute role="patient">
+            <div>Patient Dashboard</div>
+          </ProtectedRoute>
+        }
+       />
+
+       
+    </Routes>
+  );
+};
